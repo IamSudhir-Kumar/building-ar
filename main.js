8d681d800ec18ecf2ef19922a0fa8b0c1c371d3f
SUDHIR KUMAR <sudhirk33835@gmail.com>
2024-07-25 17:39:56 +0530

    chore: Fix typo in imageTargetSrc file path

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './build.mind',
+      imageTargetSrc: './buil.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
